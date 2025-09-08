dd528f26ace4b4e74b8efada525c6062d4728d13
Mateo del Rio <mdelriolanse@gmail.com>
2025-09-08 01:11:03 -0400

    feat: add multidimensional data structures and remove hardcoded NUM_FEATURES

--- regression_utils.h
+++ regression_utils.h
@@ -5,12 +5,11 @@
 
 #define EXIT_SUCCESS 0
 #define EXIT_FAILURE 1
-#define NUM_FEATURES 1
-#define LEARNING_RATE 0.1
+#define LEARNING_RATE 0.0001
 #define MAX_EPOCHS 2500
 #define MIN_EPOCHS 1
 #define MAX_SAMPLES 1000
-#define MAX_FEATURES 10 // for now only one really.
+#define MAX_FEATURES 10
 #define PATH_BUF_LEN 256
 
 //STRUCT DECLARATIONS
@@ -20,21 +19,22 @@ typedef struct {
 }RegressionParameters, *pRegressionParameters;
 
 typedef struct {
-	float *samples;
+	float **samples;  // 2D array: samples[feature][sample_index]
 	float *labels;
 	size_t length;
+	size_t num_features;
 }DataLoader, *pDataLoader;
 
 typedef struct {
 	float mse;
 	float sum_err;
-	float weighted_err;
+	float *weighted_err;  // Array for each feature's weighted error
 }TrainingDiagnostics, *pTrainingDiagnostics;
 
 //FUNCTION DECLARATIONS
 
 //initializations
-pRegressionParameters initialize_params();
+pRegressionParameters initialize_params(size_t num_features);
 int initialize_dataloader(pDataLoader dataloader, size_t num_samples, size_t num_features);
 
 //validations
@@ -46,6 +46,7 @@ void validate_path(char *buffer, char *path);
 void train(pDataLoader data, size_t epochs);
 void forward_pass(pDataLoader data, pRegressionParameters params, pTrainingDiagnostics diagnostics);
 void backward_pass(pDataLoader data, pRegressionParameters params, pTrainingDiagnostics diagnostics);
+void clear_diagnostics(pTrainingDiagnostics diagnostics, size_t num_features);
 
 //csv parser
 int csv_parser(char *path, pDataLoader dataloader, size_t nsamples, size_t nfeatures);
