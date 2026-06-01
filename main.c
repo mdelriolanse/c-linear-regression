3ea851d6fea7114d954f5966082983dc6b432d06
mdelriolanse <mdelriolanse@gmail.com>
2026-06-01 19:46:00 -0400

    refactor: persisted addition of TrainingConfig.
    main.c entrypoint now:
    - initializes TrainingConfig (ln 16 and 48)
    - correctly parses and validates learning rate user argument (ln 27)

--- main.c
+++ main.c
@@ -6,13 +6,14 @@
 
 int main(int argc, char **argv) {
 
-	if (argc != 5) {
-		printf("Usage: %s <num_epochs> <num_features> <num_samples> <data_file_path>\n", argv[0]);
-		printf("Example: %s 1000 2 100 sample_data.csv\n", argv[0]);
+	if (argc != 6) {
+		printf("Usage: %s <num_epochs> <num_features> <num_samples> <learning_rate> <data_file_path>\n", argv[0]);
+		printf("Example: %s 1000 2 100 0.001 sample_data.csv\n", argv[0]);
 		exit(EXIT_FAILURE);
 	}
 
 	pDataLoader data_loader = malloc(sizeof(DataLoader));
+	pTrainingConfig training_cfg = malloc(sizeof(TrainingConfig));
 
 	size_t num_epochs;
 	size_t num_features;
@@ -21,6 +22,12 @@ int main(int argc, char **argv) {
 
 	// Parse number of epochs
 	num_epochs = validate_epochs(argv[1]);
+
+	// Parse learning rate
+	float learning_rate = validate_learning_rate(argv[4]);
+
+	// Validate and set data file path
+	validate_path(pathbuf, argv[5]);
 	
 	// Parse number of features
 	num_features = (size_t)atoi(argv[2]);
@@ -38,13 +45,14 @@ int main(int argc, char **argv) {
 	}
 	printf("Using %zu samples\n", num_samples);
 
-	// Validate and set data file path
-	validate_path(pathbuf, argv[4]);
+  initialize_dataloader(data_loader, num_samples, num_features);
+	initialize_training_config(training_cfg, num_epochs, learning_rate);
 
-	// Parse CSV data
+	// Parse CSV data and load into the dataloader.
 	csv_parser(pathbuf, data_loader, num_samples, num_features);
 
-	train(data_loader, num_epochs);
+	// Initialize linear regression training.
+	train(data_loader, training_cfg);
 
 	return EXIT_SUCCESS;
 }
