3825239534c2fb5d693135ebf640f4d29d6490b0
mdelriolanse <mdelriolanse@gmail.com>
2026-06-01 20:48:09 -0400

    refactor: DIE macro and user-inputted learning rate parsing
    Error Checking:
    - DIE macro declaration for more robust error checking used throughout the
      project.
    
    User-Adjusted Learning Rate Overhead:
    - TrainingConfig (holds learning rate and epoch parameters) struct declaration and initialization function
      declaration.
    - validate_learning_rate to validate user-inputted learning rate at
      program entrypoint.
    - Added learning rate as parameter passed to the train() and
      backward_pass() functions.

--- regression_utils.h
+++ regression_utils.h
@@ -2,15 +2,28 @@
 #define UTILS_H
 
 #include <stddef.h>
+#include <errno.h>
 
 #define EXIT_SUCCESS 0
 #define EXIT_FAILURE 1
+
 #define LEARNING_RATE 0.0001
+#define MAX_LR 1
+#define MIN_LR 1e-12
+
 #define MAX_EPOCHS 2500
 #define MIN_EPOCHS 1
+
 #define MAX_SAMPLES 1000
 #define MAX_FEATURES 10
 #define PATH_BUF_LEN 256
+
+// Error Handling Macro
+#define DIE(msg) do { \
+	fprintf(stderr, "%s:%d: %s: ", __FILE__, __LINE__, __func__); \
+	perror(msg); \
+	exit(EXIT_FAILURE); \
+} while (0)
 
 //STRUCT DECLARATIONS
 typedef struct {
@@ -31,21 +44,28 @@ typedef struct {
 	float *weighted_err;  // Array for each feature's weighted error
 }TrainingDiagnostics, *pTrainingDiagnostics;
 
+typedef struct {
+	size_t epochs;
+	float learning_rate;
+}TrainingConfig, *pTrainingConfig;
+
 //FUNCTION DECLARATIONS
 
 //initializations
 pRegressionParameters initialize_params(size_t num_features);
 int initialize_dataloader(pDataLoader dataloader, size_t num_samples, size_t num_features);
+int initialize_training_config(pTrainingConfig cfg, size_t epochs, float learning_rate);
 
 //validations
 void validate_initialization(); // check num_features = num_labels
 size_t validate_epochs(char *nepochs);
 void validate_path(char *buffer, char *path);
+float validate_learning_rate(char *learning_rate);
 
 //training
-void train(pDataLoader data, size_t epochs);
+void train(pDataLoader data, pTrainingConfig cfg);
 void forward_pass(pDataLoader data, pRegressionParameters params, pTrainingDiagnostics diagnostics);
-void backward_pass(pDataLoader data, pRegressionParameters params, pTrainingDiagnostics diagnostics);
+void backward_pass(pDataLoader data, pRegressionParameters params, pTrainingDiagnostics diagnostics, float learning_rate);
 void clear_diagnostics(pTrainingDiagnostics diagnostics, size_t num_features);
 
 //csv parser
